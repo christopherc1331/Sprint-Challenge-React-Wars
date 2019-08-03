f73ef168e4e66f5ad75be03abaa4c0730a74c83c
Chris Cruz <christopherc1331@gmail.com>
2019-08-02 19:33:04 -0700

    Finished axios get function; need to incorporate into virtual DOM

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const randMin = 1;
+  const randMax = 30;
+  const randomNum = Math.floor(randMin + Math.random() * (randMax - randMin));
+
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState(`people/${randomNum}/`);
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://swapi.co/api/" + query)
+        .then(result => {
+          setData(result.data);
+          console.log(result.data);
+        })
+        .catch(err => console.log(err));
+    };
+
+    fetchData();
+  }, [query]);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
