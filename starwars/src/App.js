4cd1e03c043973567d432ea3fbc2d63455d1d818
Chris Cruz <christopherc1331@gmail.com>
2019-08-02 20:55:05 -0700

    What a FAIL!  I'm so bummed out!

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
-
+import "./App.css";
+import CharacterCard from "./components/CharacterCard.js";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,30 +9,40 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const randMin = 1;
-  const randMax = 30;
-  const randomNum = Math.floor(randMin + Math.random() * (randMax - randMin));
 
   const [data, setData] = useState({ hits: [] });
-  const [query, setQuery] = useState(`people/${randomNum}/`);
+  const [query, setQuery] = useState(`people/`);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     const fetchData = () => {
       axios
         .get("https://swapi.co/api/" + query)
         .then(result => {
-          setData(result.data);
-          console.log(result.data);
+          setData(result.data.results);
+          console.log(result.data.results);
         })
         .catch(err => console.log(err));
     };
 
     fetchData();
-  }, [query]);
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div>
+        {data[1].name}
+        {/* {data.results.map(item => {
+          // return <CharacterCard name={name} />;
+          console.log(item.name);
+        })} */}
+        {/* <div>
+          {data.map(item => {
+            return <p>item</p>;
+          })}
+        </div> */}
+      </div>
     </div>
   );
 };
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const CharacterCard = props => {
+  return <div>{props.name}</div>;
+};
+
+export default CharacterCard;
